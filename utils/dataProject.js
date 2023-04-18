08b061a455134efe49573aec6b18af0e4218590c
Rafa Mojica <rafael.mojica27@gmail.com>
2023-04-18 18:09:40 -0500

    Se actualiza data de project

--- utils/dataProject.js
+++ utils/dataProject.js
@@ -1,21 +1,51 @@
 const dataProjects = [
   {
     name: "Portafolio",
+    type: "Aplicacíon Web",
+    projectUrl: "/Porfolio",
     image: "/../public/assets/projects/LandpagePorfolio.png",
-    desciption:"Portafolio Rafael Mojica",
-    projectUrl: "/"
+    sumamary: "Portafolio profesional Rafael Mojica",
+    technologies: ["Next.js", "React.js", "Tailwind", "Javascript" ],
+    backUrl: "/",
+    frontUrl: "https://github.com/RafaMojica/FRONT-Porfolio",
+    demoUrl: "/",
+    webUrl: "/",
   },
   {
-    name: "Whacht Me",
+    name: "Whatch Me",
+    type: "Aplicacíon Web",
+    projectUrl: "/WhatchMe",
     image: "/../public/assets/projects/LandpageWatchMe.png",
-    desciption:"Sitio Web con tematica de películas y series",
-    projectUrl: "/"
+    sumamary: "Sitio Web enfocado en peliculas y series de television",
+    technologies: ["React.js", "Redux", "Javascript", "Node.js", "Express", "Sequelize", "PostgreSQL", "Sass" ],
+    backUrl: "https://github.com/RafaMojica/BACK-Web-WatchMe",
+    frontUrl: "https://github.com/RafaMojica/FRONT-Web-WatchMe",
+    demoUrl: "/",
+    webUrl: "/",
+  },
+  {
+    name: "RRHH",
+    type: "Aplicacíon Movil",
+    projectUrl: "/RRHH",
+    image: "/../public/assets/projects/LandpageRrhh.png",
+    sumamary: "Aplicacíon Móvil para la dependencia de recursos Humanos",
+    technologies: ["React Native", "Redux", "Javascript", "Node.js", "Express", "Sequelize", "PostgreSQL", "Material UI" ],
+    backUrl: "https://github.com/RafaMojica/BACK-App-RRHH",
+    frontUrl: "https://github.com/RafaMojica/FRONT-App-RRHH",
+    demoUrl: "/",
+    webUrl: "/",
   },
   {
     name: "Como en Casa",
+    type: "Aplicacíon Web",
+    projectUrl: "/ComoEnCasa",
     image: "/../public/assets/projects/LandpageComoEnCasa.png",
-    desciption: "Sitio web E-commerce funcional con temática de restaurante",
-    projectUrl: "/"
+    sumamary: "Sitio web E-commerce con temática de restaurante",
+    technologies: ["React.js", "Redux", "Javascript", "Node.js", "Express", "Sequelize", "PostgreSQL", "Sass" ],
+    backUrl: "https://github.com/RafaMojica/Como-en-casa/tree/main/api",
+    frontUrl: "https://github.com/RafaMojica/Como-en-casa",
+    demoUrl: "/",
+    webUrl: "/",
   },
 ];
 
