8d9ccc0dd0f96bcb1827a4173f315a0abb63ca01
Rafa Mojica <rafael.mojica27@gmail.com>
2023-04-19 14:34:11 -0500

    FEATURE: Creación footer

---
+++ components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div id="contact" className="w-full px-2 py-8">
+      <div className="max-w-[1240px] mx-auto flex items-center justify-center">
+        <p className="text-gray500">Copyrighr © 2023 Rafael Mojica. All Rights Reserved</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Navbar from "../components/Navbar";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -21,6 +22,7 @@ export default function Home() {
       <Skills />
       <Projects />
       <Contact />
+      <Footer />
     </>
   );
 }
