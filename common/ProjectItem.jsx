d5f40e9580aaabff33f9c07efbf1de47bd9f545f
Rafa Mojica <rafael.mojica27@gmail.com>
2023-04-21 13:46:05 -0500

    FIX Se cambia la opacidad de la Imagen

--- common/ProjectItem.jsx
+++ common/ProjectItem.jsx
@@ -6,7 +6,7 @@ const ProjectItem = ({ project }) => {
   const { image, name, sumamary, projectUrl } = project
   return (
     <div className="relative p-4 flex items-center justify-center h-auto w-full shadow-xl shadow-shodowGrey400 rounded-xl group hover:bg-gradient-to-b from-secondary to-background md:hover:scale-105 ease-in duration-300">
-      <Image className="rounded-xl group-hover:opacity-0" src={image} alt={`Proyecto ${name}`} width="800" height="800"/>
+      <Image className="rounded-xl group-hover:opacity-10" src={image} alt={`Proyecto ${name}`} width="800" height="800"/>
       <div className="hidden group-hover:block absolute w-[80%]">
         <h3 className="text-2xl tracking-wider text-center">{name}</h3>
         <p className="pb-4 pt-2 text-center">{sumamary}</p>
