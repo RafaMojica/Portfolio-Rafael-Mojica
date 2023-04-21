a1aebdd586e72b75cc7d059dec9947a9ed64d843
Rafa Mojica <rafael.mojica27@gmail.com>
2023-04-21 15:47:21 -0500

    FEATURE: Se agrega animacion del nombre

--- components/Start.jsx
+++ components/Start.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import SocialNetworks from "../common/SocialNetworks";
 import {BsArrowDown} from "react-icons/bs"
 import Link from "next/link";
+import Typed from 'typed.js';
 
 const Start = () => {
   const [arrow, setArrow] = useState(true);
@@ -17,14 +18,27 @@ const Start = () => {
     window.addEventListener('scroll', handleArrow);
   }, []);
 
+  useEffect(() => {
+    const typed = new Typed("#name", {
+      strings: ['Rafael Mojica'],
+      typeSpeed: 150,
+      backSpeed: 150,
+      startDelay: 400,
+      backDelay: 2000,
+      loop: true,
+    });
+
+    return () => typed.destroy();
+  }, []);
+
   return (
     <div id="start" className="w-full h-screen text-center">
       <div className="relative max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
-          <p className="uppercase text-base tracking-widest text-gray600">Logremos algo juntos</p>
-          <h1 className="py-4 text-gray700">Hola, soy <span className="text-secondary"> Rafael Mojica</span></h1>
+          <p className="uppercase text-sm sm:text-base tracking-widest text-gray600">Logremos algo juntos</p>
+          <h1 className="py-4 text-gray700">Hola, soy <span id="name" className="text-secondary"></span></h1>
           <h1 className="py-2 text-gray700">Full-Stack Web Developer</h1>
-          <p className="py-4 text-gray600 sm:max-w-[70%] m-auto text-lg">
+          <p className="py-4 text-gray600 sm:max-w-[70%] m-auto text-sm sm:text-lg">
             A través de este espacio, espero poder compartir contigo mi pasión,
             experiencia y conocimientos en el mundo IT.
           </p>
