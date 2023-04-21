78182b7e12db2996f10e2bf4f6cba090c2db9eaf
Rafa Mojica <rafael.mojica27@gmail.com>
2023-04-21 13:46:28 -0500

    FEATURE: Se agrega flech de scroll

--- components/Start.jsx
+++ components/Start.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SocialNetworks from "../common/SocialNetworks";
+import {BsArrowDown} from "react-icons/bs"
+import Link from "next/link";
 
 const Start = () => {
+  const [arrow, setArrow] = useState(true);
+
+  useEffect(() => {
+    const handleArrow = () => {
+      if (window.scrollY >= 90) {
+        setArrow(false);
+      } else {
+        setArrow(true);
+      }
+    };
+    window.addEventListener('scroll', handleArrow);
+  }, []);
+
   return (
     <div id="start" className="w-full h-screen text-center">
-      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+      <div className="relative max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-base tracking-widest text-gray600">Logremos algo juntos</p>
           <h1 className="py-4 text-gray700">Hola, soy <span className="text-secondary"> Rafael Mojica</span></h1>
@@ -16,6 +31,11 @@ const Start = () => {
           <div className="flex items-center justify-between max-w-[400px] m-auto py-4">
             <SocialNetworks size={20} padding={6} />
           </div>
+        </div>
+        <div className={arrow ? "absolute left-[45%] md:left-[48%] bottom-5" : "hidden"}>
+          <Link href="/#about">
+              <BsArrowDown className="animate-bounce text-secondary" size={35} />
+          </Link>
         </div>
       </div>
     </div>
