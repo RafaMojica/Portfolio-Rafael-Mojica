a75a3f09528036f716c204035866abf5f0df90da
Rafa Mojica <rafael.mojica27@gmail.com>
2023-04-23 15:33:22 -0500

    FIXBUG: Animacion en rutas -pages

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,8 @@ import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
-import { useEffect } from "react";
-import Aos from "aos";
 
 export default function Home() {
-  
-  useEffect(()=>{
-  Aos.init({
-    duration: 2000
-  });
-  },[])
 
   return (
     <>
