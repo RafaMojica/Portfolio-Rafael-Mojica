43d4c4c9abe30630a5bc505aced009dc9d22a01a
Rafa Mojica <rafael.mojica27@gmail.com>
2023-04-24 22:47:40 -0500

    BUGFIX: Corrección de animaciones

--- pages/RRHH.jsx
+++ pages/RRHH.jsx
@@ -12,7 +12,7 @@ const RRHH = () => {
     <div className="w-full">
       <HeaderProject name={name} image={image} type={type} />
       <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-4 gap-8 py-6">
-        <div data-aos="fade-right" className="col-span-4 md:col-span-3">
+        <div data-aos="zoom-in" className="col-span-4 md:col-span-3">
           <h2 className="py-4">Descripción</h2>
           <p>
             App Móvil para la empresa GlobalNewsGroup como proyecto profesional
@@ -40,7 +40,7 @@ const RRHH = () => {
           </p>
           <DescriptionButton frontUrl={frontUrl} backUrl={backUrl} demoUrl={demoUrl} webUrl={webUrl} />
         </div>
-        <div data-aos="fade-left" className="col-span-4 md:col-span-1 shadow-xl shadow-shodowGrey400 rounded-xl p-6">
+        <div data-aos="zoom-in" className="col-span-4 md:col-span-1 shadow-xl shadow-shodowGrey400 rounded-xl p-6">
           <p className="text-center font-bold pb-2">Tecnologías</p>
           {technologies.map((technologie) => (
             <Technologies key={technologie} technologie={technologie} />
