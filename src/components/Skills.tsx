6934e77b2065991b0d2152794e6d9b4bf4a3cbde
Rafael Mojica <rafael.mojica27@gmail.com>
2024-02-29 17:50:42 -0500

    Feat: Migration Skills component a typescript

---
+++ src/components/Skills.tsx
@@ -0,0 +1,23 @@
+import Layout from "@/common/Layout";
+import IconButton from "@/common/IconButton";
+import stack from "@/data/Skills";
+import React from "react";
+
+const Skills = () => {
+  return (
+    <Layout id="skills">
+      <h2 className="text-center text-secondary font-bold">Stack</h2>
+      <ul className="flex flex-wrap justify-center items-center gap-5 pt-12">
+        {stack.map(({ name, href, Icon }) => {
+          return (
+            <IconButton key={name} href={href}>
+              <Icon />
+            </IconButton>
+          );
+        })}
+      </ul>
+    </Layout>
+  );
+};
+
+export default Skills;
