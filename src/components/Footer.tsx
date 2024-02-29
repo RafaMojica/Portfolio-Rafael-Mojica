443f57ac86f674ccb1490583e62a67d2ed46eaaf
Rafael Mojica <rafael.mojica27@gmail.com>
2024-02-29 17:45:40 -0500

    Feat: migration footer a typescript

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/common/Layout";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <Layout id="contact" className="pt-11 pb-6 md:pt-24">
+      <p className="text-gray400 text-sm sm:text-base flex items-center justify-center">
+        Copyright © 2023 Rafael Mojica. All Rights Reserved
+      </p>
+    </Layout>
+  );
+};
+
+export default Footer;
