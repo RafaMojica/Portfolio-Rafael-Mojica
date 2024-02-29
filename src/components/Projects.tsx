7909a5260885ef7f8983154521a5efe707df452d
Rafael Mojica <rafael.mojica27@gmail.com>
2024-02-29 18:06:59 -0500

    Fix: change route of import

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,17 +1,19 @@
 import React from "react";
-import ProjectItem from "../common/ProjectItem";
-import dataProject from "../utils/dataProject"
+// import ProjectItem from "../common/ProjectItem";
+// import dataProject from "../utils/dataProject"
 
 const Projects = () => {
   return (
     <div id="projects" className="w-full px-2 py-16 md:py-24">
       <div className="max-w-[1240px] mx-auto">
-        <p className="uppercase text-xl tracking-widest text-secondary font-bold">Proyectos</p>
+        <p className="uppercase text-xl tracking-widest text-secondary font-bold">
+          Proyectos
+        </p>
         <h2 className="py-4">¿Qué he desarrollado?</h2>
         <div className="grid md:grid-cols-2 gap-8">
-          {dataProject.map( project =>
+          {/* {dataProject.map( project =>
             <ProjectItem key={project.name} project={project} />
-          )}
+          )} */}
         </div>
       </div>
     </div>
