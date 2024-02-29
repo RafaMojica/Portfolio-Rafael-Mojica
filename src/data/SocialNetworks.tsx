6ac02aed39657f83448ed991e1c9d0704a1e2327
Rafael Mojica <rafael.mojica27@gmail.com>
2024-02-29 17:44:33 -0500

    feat: creation file data

---
+++ src/data/SocialNetworks.tsx
@@ -0,0 +1,37 @@
+import { Linkedin, Github, Instagram, Mail, Mobil } from "@/common/Icons";
+import { IconDescription } from "@/types/icon.types";
+
+const socialNetworks: IconDescription[] = [
+  {
+    name: "linkedin",
+    Icon: Linkedin,
+    color: "#000000",
+    href: "https://www.linkedin.com/in/rafamojica/",
+  },
+  {
+    name: "github",
+    Icon: Github,
+    color: "#000000",
+    href: "https://github.com/RafaMojica",
+  },
+  {
+    name: "instagram",
+    Icon: Instagram,
+    color: "#000000",
+    href: "https://instagram.com/rafa_mojica_",
+  },
+  {
+    name: "mail",
+    Icon: Mail,
+    color: "#000000",
+    href: "mailto:rafael.mojica27@gmail.com",
+  },
+  {
+    name: "mobil",
+    Icon: Mobil,
+    color: "#000000",
+    href: "https://wa.link/r6856s",
+  },
+];
+
+export default socialNetworks;
