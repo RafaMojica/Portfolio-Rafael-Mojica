dec3fe35624ea69a77932d5882bacee0f90621e3
Rafael Mojica <rafael.mojica27@gmail.com>
2024-03-04 14:09:19 -0500

    Fix: Name section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,7 +6,7 @@ import React from "react";
 const Skills = () => {
   return (
     <Layout id="skills">
-      <h2 className="text-center text-secondary font-bold">Stack</h2>
+      <h2 className="text-center text-secondary font-bold">Habilidades</h2>
       <ul className="flex flex-wrap justify-center items-center gap-5 pt-12">
         {stack.map(({ name, href, Icon }) => {
           return (
