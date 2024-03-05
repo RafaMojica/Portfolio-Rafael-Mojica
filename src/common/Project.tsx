aeaa87d0a1083223df470b4eb11438be54cd430a
Rafael Mojica <rafael.mojica27@gmail.com>
2024-03-05 13:29:46 -0500

    fix: file reorganization

---
+++ src/common/Project.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const ProjectItem = () => {
+  // const { image, name, projectUrl } = project;
+
+  return (
+    <></>
+    // <div className="relative p-4 flex flex-col items-center justify-center h-auto w-full shadow-xl shadow-shodowGrey400 rounded-xl group hover:bg-gradient-to-b from-secondary to-background md:hover:scale-105 ease-in duration-300">
+    //   <Image
+    //     className="rounded-xl group-hover:opacity-10"
+    //     src={image}
+    //     alt={`Proyecto ${name}`}
+    //     width="800"
+    //     height="800"
+    //   />
+    //   <Link className="flex justify-center" href={projectUrl}>
+    //     <p className="p-4 mt-4 bg-buttonBgProject font-bold shadow-xl shadow-shodowGrey400 rounded-xl hover:bg-secondary ease-in duration-300">
+    //       Ver más
+    //     </p>
+    //   </Link>
+    // </div>
+  );
+};
+
+export default ProjectItem;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,22 +1,20 @@
 import React from "react";
+import Layout from "@/common/Layout";
 // import ProjectItem from "../common/ProjectItem";
 // import dataProject from "../utils/dataProject"
 
 const Projects = () => {
   return (
-    <div id="projects" className="w-full px-2 py-16 md:py-24">
+    <Layout id="projects">
       <div className="max-w-[1240px] mx-auto">
-        <p className="uppercase text-xl tracking-widest text-secondary font-bold">
-          Proyectos
-        </p>
-        <h2 className="py-4">¿Qué he desarrollado?</h2>
+        <h2 className="text-center text-secondary font-bold">Proyectos</h2>
         <div className="grid md:grid-cols-2 gap-8">
           {/* {dataProject.map( project =>
             <ProjectItem key={project.name} project={project} />
           )} */}
         </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
