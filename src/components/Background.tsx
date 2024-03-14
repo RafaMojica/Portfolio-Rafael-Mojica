42be6ad48fa543249b59bb165380b9c5875b6b71
Rafael Mojica <rafael.mojica27@gmail.com>
2024-03-14 12:46:13 -0500

    fix: background mobil

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -7,9 +7,9 @@ const Background = () => {
         <div className="absolute left-[1100px] right-0 top-[150px] -z-30 m-auto h-[410px] w-[410px] rounded-full bg-blue-500 dark:bg-blue-600 opacity-20 dark:opacity-30 blur-[110px]"></div>
         <div className="absolute left-0 right-[1150px] top-[400px] -z-30 m-auto h-[510px] w-[510px] rounded-full bg-blue-500 dark:bg-blue-600 opacity-20 dark:opacity-30 blur-[110px]"></div>
         <div
-          className="fixed inset-0 -z-30 h-full w-full bg-[size:6rem_4rem] opacity-60
+          className="fixed inset-0 -z-30 h-full w-full bg-[size:6rem_4rem] opacity-10 md:opacity-60
           bg-[linear-gradient(to_right,#f0f0f0,transparent_1px),linear-gradient(to_bottom,#f0f0f0,transparent_1px)] 
-          dark:bg-[linear-gradient(to_right,#274C89,transparent_1px),linear-gradient(to_bottom,#274C89,transparent_1px)] dark:opacity-40"
+          dark:bg-[linear-gradient(to_right,#274C89,transparent_1px),linear-gradient(to_bottom,#274C89,transparent_1px)] md:dark:opacity-30"
         ></div>
       </div>
     </>
