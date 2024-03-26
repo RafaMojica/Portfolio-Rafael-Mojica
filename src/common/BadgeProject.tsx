fe6a2a6e8519686af7beda1aa882c6f99f97c30b
Rafael Mojica <rafael.mojica27@gmail.com>
2024-03-26 15:55:08 -0500

    fix: Color button repository

--- src/common/BadgeProject.tsx
+++ src/common/BadgeProject.tsx
@@ -14,7 +14,7 @@ const BadgeProject: FC<BadgeProjectProps> = ({ Icon, title, href }) => {
       target={href.startsWith("http") ? "_blank" : undefined}
       rel={href.startsWith("http") ? "noreferrer" : undefined}
       className="flex items-center justify-center px-4 py-2 rounded-xl gap-1 hover:scale-105 transition ease-in-out duration-300 font-medium border-2 border-transparent outline-transparent
-      text-white bg-gray-700 hover:bg-black
+      text-white bg-gray-700/50 hover:bg-black
       dark:text-white dark:bg-gray-700 dark:hover:bg-black hover:border-secondary hover:outline-secondary
       "
     >
