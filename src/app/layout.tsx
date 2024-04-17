7add6de82c99a8d73de498d134c5b5006caf12ec
Rafael Mojica <rafael.mojica27@gmail.com>
2024-04-17 16:37:51 -0500

    feat: add new font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,17 @@
 import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import Background from "@/components/Background";
 
 export const metadata: Metadata = {
   title: "Portafolio | Rafael Mojica",
-  description: "Portafolio Rafael Mojica",
+  description:
+    "Portafolio personal web donde conocerás un poco más sobre mi trayectoria en el mundo del desarrollo web, aquí podrás ver mi experiencia laboral, proyectos realizados, tecnologías que manejo y mucho más.",
 };
+
+const roboto = Roboto({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -16,11 +20,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body>
-        <Background />
-        <Header />
+      <body className={`bg-dBg ${roboto.className} antialiased`}>
         {children}
-        <Footer />
       </body>
     </html>
   );
