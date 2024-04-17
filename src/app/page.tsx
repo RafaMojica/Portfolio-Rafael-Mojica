7e7c92031ee0f668fb725c28cdd9c19821cc273b
Rafael Mojica <rafael.mojica27@gmail.com>
2024-04-17 16:39:57 -0500

    feat: delete old background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 // import About from "@/components/About";
+import Navbar from "@/components/Navbar";
 import Introduction from "@/components/Introduction";
-import Skills from "../components/Skills";
 import Projects from "@/components/Projects";
+import Skills from "../components/Skills";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
       <Introduction />
       {/* <About /> */}
       <Projects />
       <Skills />
+      <Footer />
     </main>
   );
 }
