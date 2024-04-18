44a0fb573be5f44ad9452462a6119fc56c7444db
Rafael Mojica <rafael.mojica27@gmail.com>
2024-04-17 20:20:00 -0500

    feat: new navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,170 @@
+"use client";
+
+import Link from "next/link";
+import React, { useState } from "react";
+import useScroll from "@/hook/useScroll";
+import IconButton from "@/common/IconButton";
+import Layout from "@/common/Layout";
+import { RM } from "@/icons/RM";
+import { Menu } from "@/icons/Menu";
+import { CloseX } from "@/icons/CloseX";
+import { Sun } from "@/icons/Sun";
+import { Moon } from "@/icons/Moon";
+import { FlagCol } from "@/icons/FlagCol";
+import { FlagUsa } from "@/icons/FlagUsa";
+import { SOCIAL_NETWORKS_INTRODUCTION } from "@/data/socialNetworks";
+
+const Navbar = () => {
+  const scroll = useScroll(90);
+  const [nav, setNav] = useState(false);
+  const [theme, setTheme] = useState("dark");
+  const [language, setLanguage] = useState("spanish");
+
+  // useEffect(() => {
+  //   theme === "dark"
+  //     ? document.documentElement.classList.add("dark")
+  //     : document.documentElement.classList.remove("dark");
+  // }, [theme]);
+
+  return (
+    <header
+      className={
+        !scroll
+          ? "fixed w-full h-16 z-[100] backdrop-blur-xl shadow-xl shadow-dGradientPrimary/10"
+          : "fixed w-full h-16 z-[100]"
+      }
+    >
+      <Layout
+        id="navbarDesktop"
+        className="h-full flex justify-between items-center"
+      >
+        <div className="flex h-full justify-start items-center lg:basis-40">
+          <Link href="/" aria-label="Logo portafolio web">
+            <RM className="text-white" />
+          </Link>
+        </div>
+        <nav className="flex items-center justify-center gap-4 md:gap-8">
+          <button
+            onClick={() => {
+              setLanguage((prevTheme) =>
+                prevTheme === "spanish" ? "english" : "spanish"
+              );
+            }}
+          >
+            {language === "spanish" ? <FlagUsa /> : <FlagCol />}
+          </button>
+          <ul className="hidden md:flex gap-8 justify-center items-center">
+            <li className="UnderlineEffect">
+              <Link href="/">Inicio</Link>
+            </li>
+            <li className="UnderlineEffect">
+              <Link href="/#experience">Experiencia</Link>
+            </li>
+            <li className="UnderlineEffect">
+              <Link href="/#projects">Proyectos</Link>
+            </li>
+            <li className="UnderlineEffect">
+              <Link href="/#aboutMe">Sobre mí</Link>
+            </li>
+            <li className="UnderlineEffect">
+              <Link href="/#skills">Habilidades</Link>
+            </li>
+          </ul>
+          <button
+            onClick={() => {
+              setTheme((prevTheme) =>
+                prevTheme === "light" ? "dark" : "light"
+              );
+            }}
+          >
+            {theme === "light" ? <Moon /> : <Sun />}
+          </button>
+          <button className="md:hidden" onClick={() => setNav(!nav)}>
+            <Menu />
+          </button>
+        </nav>
+        <div className="hidden md:flex h-full justify-end items-center lg:basis-40">
+          <Link
+            href="https://drive.google.com/file/d/1Pe-UKjdPGVZC53y1DmgpNwx3PUDryUd4/view"
+            target="_blank"
+            className="px-7 py-3 rounded-full font-medium text-dSecondary
+          bg-gradient-to-b from-dGradientPrimary/70 to-dGradientSecondary/70
+          hover:from-dGradientPrimary hover:to-dGradientSecondary"
+          >
+            Descargar CV
+          </Link>
+        </div>
+      </Layout>
+
+      {/* Mobile Menu */}
+
+      <section
+        className={
+          nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""
+        }
+      >
+        <div
+          className={
+            nav
+              ? "fixed left-0 top-0 w-[75%] p-7 ease-in duration-500 sm:w-[60%] md:w-[45%] h-screen bg-dBg"
+              : "fixed left-[-100%] top-0 p-7 ease-in duration-500"
+          }
+        >
+          <div className="flex w-full items-center justify-between">
+            <Link href="/">
+              <RM className="text-white" />
+            </Link>
+            <button onClick={() => setNav(!nav)}>
+              <CloseX />
+            </button>
+          </div>
+          <div className="mt-5 text-dSecondary border-b border-dGradientPrimary">
+            <p className="w-[85%] md:w-[98%] py-2 text-sm">
+              Construyamos algo juntos
+            </p>
+          </div>
+          <nav className="flex flex-col mt-9 gap-6">
+            <ul className="flex flex-col text-sm gap-5">
+              <li className="text-dSecondary">
+                <Link onClick={() => setNav(false)} href="/">
+                  Inicio
+                </Link>
+              </li>
+              <li className="text-dSecondary">
+                <Link onClick={() => setNav(false)} href="/#experience">
+                  Experiencia
+                </Link>
+              </li>
+              <li className="text-dSecondary">
+                <Link onClick={() => setNav(false)} href="/#projects">
+                  Proyectos
+                </Link>
+              </li>
+              <li className="text-dSecondary">
+                <Link onClick={() => setNav(false)} href="/#aboutMe">
+                  Sobre mí
+                </Link>
+              </li>
+              <li className="text-dSecondary">
+                <Link onClick={() => setNav(false)} href="/#skills">
+                  Habilidades
+                </Link>
+              </li>
+            </ul>
+            <div className="flex flex-wrap items-center gap-3 justify-center my-4 mx-auto w-[85%]">
+              {SOCIAL_NETWORKS_INTRODUCTION.map(({ name, href, Icon }) => {
+                return (
+                  <IconButton key={`${name}`} href={`${href}`}>
+                    <Icon width="24" height="24" />
+                  </IconButton>
+                );
+              })}
+            </div>
+          </nav>
+        </div>
+      </section>
+    </header>
+  );
+};
+
+export default Navbar;
