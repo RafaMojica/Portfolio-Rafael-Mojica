cf06f3c1540bcea05c2811bfc7248341cf41a2b3
Rafael Mojica <rafael.mojica27@gmail.com>
2024-04-18 23:04:16 -0500

    feat: creation new introduction

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -12,26 +12,30 @@ const Introduction = () => {
   const scroll = useScroll(90);
 
   return (
-    <Layout id="introduction" className="h-screen">
-      <div className="relative h-screen flex justify-center items-center text-center">
-        <header className="flex flex-col gap-6 col-span-2">
-          <p className="uppercase text-sm sm:text-base tracking-widest text-description dark:text-dark-description">
+    <Layout
+      id="introduction"
+      className="h-screen flex flex-col items-center justify-center"
+    >
+      <header className="mt-16 h-[calc(100vh-50px-64px)] flex items-center justify-center">
+        <div className="flex flex-col gap-6 text-center">
+          <p className="uppercase text-sm sm:text-base tracking-widest text-dPrimary">
             Construyamos algo juntos
           </p>
-          <h1 className="font-bold text-primary dark:text-dark-primary">
-            <span className="text-secondary dark:text-dark-secondary">
-              {" "}
+          <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold ">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-dGradientPrimary to-dGradientSecondary">
               Rafael Mojica
             </span>
-            <span className="pt-5 block text-primary dark:text-dark-primary">
+          </h1>
+          <h2 className="text-3xl sm:text-5xl md:text-6xl font-bold ">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-dSecondary to-dPrimary">
               Full-Stack Web Developer
             </span>
-          </h1>
-          <p className="text-description sm:max-w-[70%] m-auto text-sm sm:text-lg dark:text-dark-description">
+          </h2>
+          <p className="text-sm sm:max-w-[70%] sm:text-lg m-auto text-dPrimary">
             A través de este espacio, espero poder compartir contigo mi pasión,
             experiencia y conocimientos en el mundo IT.
           </p>
-          <div className="flex items-center justify-between gap-2 md:gap-3 max-w-[400px] m-auto py-2">
+          <div className="flex items-center justify-between gap-2 md:gap-3 max-w-[400px] m-auto">
             {SOCIAL_NETWORKS_INTRODUCTION.map(({ name, href, Icon }) => {
               return (
                 <IconButton
@@ -44,22 +48,13 @@ const Introduction = () => {
               );
             })}
           </div>
-        </header>
-        <footer
-          className={
-            scroll ? "absolute left-[41%] md:left-[47%] bottom-5" : "hidden"
-          }
-        >
-          <Link href="/#about">
-            <ArrowDown
-              width="60"
-              height="60"
-              strokeWidth="1"
-              className="animate-bounce text-secondary dark:text-dark-secondary"
-            />
-          </Link>
-        </footer>
-      </div>
+        </div>
+      </header>
+      <footer className="h-[60px]">
+        <div className={scroll ? "visible" : "hidden"}>
+          <ArrowDown className="animate-bounce" />
+        </div>
+      </footer>
     </Layout>
   );
 };
