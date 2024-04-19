fe63afce2f08fa87435192df64535dacf199592a
Rafael Mojica <rafael.mojica27@gmail.com>
2024-04-18 23:03:46 -0500

    feat: SVG optimization arrow down

--- src/icons/ArrowDown.tsx
+++ src/icons/ArrowDown.tsx
@@ -1,29 +1,49 @@
 import { IconProps } from "@/types/icon.types";
 import { FC } from "react";
 
-export const ArrowDown: FC<IconProps> = ({
-  width,
-  height,
-  strokeWidth,
-  stroke,
-  fill,
-  className,
-}) => {
+export const ArrowDown: FC<IconProps> = ({ className }) => {
   return (
     <svg
       className={`${className || ""}`}
+      width="60"
+      height="60"
+      viewBox="0 0 60 60"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      width={`${width || "24"}`}
-      height={`${height || "24"}`}
-      viewBox="0 0 24 24"
-      strokeWidth={`${strokeWidth || "1.75"}`}
-      stroke={`${stroke || "currentColor"}`}
-      fill={`${fill || "none"}`}
-      strokeLinecap="round"
-      strokeLinejoin="round"
     >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-      <path d="M17 13v-6l-5 4l-5 -4v6l5 4z" />
+      <g clip-path="url(#clip0_1_31)">
+        <path
+          d="M42.5 32.5V17.5L30 27.5L17.5 17.5V32.5L30 42.5L42.5 32.5Z"
+          stroke="url(#paint0_linear_1_31)"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <rect
+        x="0.5"
+        y="0.5"
+        width="59"
+        height="59"
+        stroke="black"
+        strokeOpacity="0.1"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_1_31"
+          x1="30"
+          y1="17.5"
+          x2="30"
+          y2="42.5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#2275EE" />
+          <stop offset="1" stopColor="#EB00FF" />
+        </linearGradient>
+        <clipPath id="clip0_1_31">
+          <rect width="60" height="60" fill="white" />
+        </clipPath>
+      </defs>
     </svg>
   );
 };
