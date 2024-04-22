ee3a8c65ce1fb234b07921ad9756aa7f6b09772e
Rafael Mojica <rafael.mojica27@gmail.com>
2024-04-22 14:31:04 -0500

    feat: added CV contact details

--- src/data/socialNetworks.data.ts
+++ src/data/socialNetworks.data.ts
@@ -3,51 +3,43 @@ import { Instagram } from "@/icons/Instagram";
 import { Linkedin } from "@/icons/Linkedin";
 import { Mail } from "@/icons/Mail";
 import { Mobil } from "@/icons/Mobil";
-import { Contact, SocialNetworks } from "@/types/socialNetworks.types";
 
-const CONTACT: Contact = {
+export const CONTACTS = {
   linkedin: {
     name: "linkedin",
     Icon: Linkedin,
-    color: "#000000",
     href: "https://www.linkedin.com/in/rafamojica/",
   },
   github: {
     name: "github",
     Icon: Github,
-    color: "#000000",
     href: "https://github.com/RafaMojica",
   },
   instagram: {
     name: "instagram",
     Icon: Instagram,
-    color: "#000000",
     href: "https://instagram.com/rafa_mojica_",
   },
   mail: {
     name: "mail",
     Icon: Mail,
-    color: "#000000",
     href: "mailto:rafael.mojica27@gmail.com",
   },
   mobil: {
     name: "mobil",
     Icon: Mobil,
-    color: "#000000",
     href: "https://wa.link/r6856s",
+  },
+  cv: {
+    name: "curriculum",
+    href: "https://drive.google.com/file/d/1Pe-UKjdPGVZC53y1DmgpNwx3PUDryUd4/view",
   },
 };
 
-export const SOCIAL_NETWORKS_INTRODUCTION: SocialNetworks[] = [
-  CONTACT.linkedin,
-  CONTACT.github,
-  CONTACT.instagram,
-  CONTACT.mail,
-  CONTACT.mobil,
-];
-
-export const SOCIAL_NETWORKS_FOOTER: SocialNetworks[] = [
-  CONTACT.linkedin,
-  CONTACT.github,
-  CONTACT.instagram,
+export const SOCIAL_NETWORKS = [
+  CONTACTS.linkedin,
+  CONTACTS.github,
+  CONTACTS.instagram,
+  CONTACTS.mail,
+  CONTACTS.mobil,
 ];
