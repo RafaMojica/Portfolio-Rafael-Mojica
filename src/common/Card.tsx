b683c4507c279b6ddce5600a2de6d9c87bc5b4d7
Rafael Mojica <rafael.mojica27@gmail.com>
2024-04-23 13:28:22 -0500

    feat: create common card

---
+++ src/common/Card.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from "react";
+
+interface CardProps {
+  children: JSX.Element[] | JSX.Element;
+  className?: string;
+}
+
+const Card: FC<CardProps> = ({ children, className }) => {
+  return (
+    <div
+      className={`p-5 sm:p-9 rounded-2xl flex flex-col items-center gap-6 h-full bg-dSecondary/5 ${
+        className || ""
+      }`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
