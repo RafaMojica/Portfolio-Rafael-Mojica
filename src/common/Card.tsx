4ca3aca0b373c5f197654dfc388b2f30a1daa684
Rafael Mojica <rafael.mojica27@gmail.com>
2024-04-23 15:30:35 -0500

    fix: add article

--- src/common/Card.tsx
+++ src/common/Card.tsx
@@ -7,13 +7,13 @@ interface CardProps {
 
 const Card: FC<CardProps> = ({ children, className }) => {
   return (
-    <div
+    <article
       className={`p-5 sm:p-9 rounded-2xl flex flex-col items-center gap-6 h-full bg-dSecondary/5 ${
         className || ""
       }`}
     >
       {children}
-    </div>
+    </article>
   );
 };
 
