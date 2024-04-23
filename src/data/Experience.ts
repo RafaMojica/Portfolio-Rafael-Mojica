c2b2fc3583f6c23562208ee523d836990ddf17d7
Rafael Mojica <rafael.mojica27@gmail.com>
2024-04-23 14:05:17 -0500

    fix: add class highlighted

--- src/data/Experience.ts
+++ src/data/Experience.ts
@@ -8,10 +8,10 @@ const COMPANIES = {
     position: "Mentor en desarrollo de sofware",
     duration: "abr 2023 - nov 2023",
     tasks: [
-      `Desempeñe el rol de <span class="font-bold text-dEmphasis">tech leader</span> en distintos equipos alcanzando los <span class="font-bold text-dEmphasis">objetivos</span> establecidos de manera <span class="font-bold text-dEmphasis">puntual</span>.`,
-      `En calidad de <span class="font-bold text-dEmphasis">scrum master</span>, lideré sesiones que impulsaron la <span class="font-bold text-dEmphasis">participación</span> y <span class="font-bold text-dEmphasis">eficiencia</span> de los equipos.`,
-      `Proporcioné <span class="font-bold text-dEmphasis">retroalimentación</span> sobre el <span class="font-bold text-dEmphasis">código</span> de los estudiantes logrando incrementar su <span class="font-bold text-dEmphasis">nivel técnico</span>.`,
-      `Realice <span class="font-bold text-dEmphasis">sesiones educativas</span> sobre distintas <span class="font-bold text-dEmphasis">tecnologias</span> asegurando el entendimiento de los estudiantes.`,
+      `Desempeñe el rol de <span class="highlightedWords">tech leader</span> en distintos equipos alcanzando los <span class="highlightedWords">objetivos</span> establecidos de manera <span class="highlightedWords">puntual</span>.`,
+      `En calidad de <span class="highlightedWords">scrum master</span>, lideré sesiones que impulsaron la <span class="highlightedWords">participación</span> y <span class="highlightedWords">eficiencia</span> de los equipos.`,
+      `Proporcioné <span class="highlightedWords">retroalimentación</span> sobre el <span class="highlightedWords">código</span> de los estudiantes logrando incrementar su <span class="highlightedWords">nivel técnico</span>.`,
+      `Realice <span class="highlightedWords">sesiones educativas</span> sobre distintas <span class="highlightedWords">tecnologias</span> asegurando el entendimiento de los estudiantes.`,
     ],
   },
   Uma: {
@@ -20,11 +20,11 @@ const COMPANIES = {
     position: "Desarrollador Full Stack",
     duration: "may 2023 - ago 2023",
     tasks: [
-      `Participé en el desarrollo de una <span class="font-bold text-dEmphasis">aplicación</span> que ofrece el servicio de <span class="font-bold text-dEmphasis">delivery</span> para repartidores.`,
-      `Cree diferentes <span class="font-bold text-dEmphasis">endpoints</span> para la <span class="font-bold text-dEmphasis">API</span> en el <span class="font-bold text-dEmphasis">backend</span>.`,
-      `Realice <span class="font-bold text-dEmphasis">vistas</span> en el <span class="font-bold text-dEmphasis">frontend</span> con su conexion al backend.`,
-      `Implementé <span class="font-bold text-dEmphasis">test unitarias</span> y de <span class="font-bold text-dEmphasis">integración</span>.`,
-      `Desarrollé la <span class="font-bold text-dEmphasis">dockerización</span>, así como la implementación de <span class="font-bold text-dEmphasis">CI/CD</span> con <span class="font-bold text-dEmphasis">Github Actions</span>.`,
+      `Participé en el desarrollo de una <span class="highlightedWords">aplicación</span> que ofrece el servicio de <span class="highlightedWords">delivery</span> para repartidores.`,
+      `Cree diferentes <span class="highlightedWords">endpoints</span> para la <span class="highlightedWords">API</span> en el <span class="highlightedWords">backend</span>.`,
+      `Realice <span class="highlightedWords">vistas</span> en el <span class="highlightedWords">frontend</span> con su conexion al backend.`,
+      `Implementé <span class="highlightedWords">test unitarias</span> y de <span class="highlightedWords">integración</span>.`,
+      `Desarrollé la <span class="highlightedWords">dockerización</span>, así como la implementación de <span class="highlightedWords">CI/CD</span> con <span class="highlightedWords">Github Actions</span>.`,
     ],
   },
 };
