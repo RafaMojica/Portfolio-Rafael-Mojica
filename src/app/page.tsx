83537126ec5595da89cba819ef9d59ed6ecfcfeb
Rafael Mojica <rafael.mojica27@gmail.com>
2024-04-23 13:29:01 -0500

    feat: create component experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 // import About from "@/components/About";
 import Navbar from "@/components/Navbar";
 import Introduction from "@/components/Introduction";
+import Experience from "@/components/Experience";
 import Projects from "@/components/Projects";
 import Skills from "../components/Skills";
 import Footer from "@/components/Footer";
@@ -10,6 +11,7 @@ export default function Home() {
     <main>
       <Navbar />
       <Introduction />
+      <Experience />
       {/* <About /> */}
       <Projects />
       <Skills />
---
+++ src/components/Experience.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Layout from "@/common/Layout";
+import MainTitle from "@/common/MainTitle";
+import Image from "next/image";
+import Card from "@/common/Card";
+import { EXPERIENCE } from "@/data/Experience";
+
+const Experience = () => {
+  return (
+    <Layout
+      id="experience"
+      className="py-11 md:py-20 flex flex-col gap-11 md:gap-20"
+    >
+      <MainTitle title="Experiencia" />
+      <div className="grid md:grid-cols-2 gap-8 xl:gap-12 justify-center items-center">
+        {EXPERIENCE.map(({ name, logo, position, duration, tasks }) => {
+          return (
+            <Card key={name}>
+              <Image src={logo} alt={`Foto Empresa ${name}`} />
+              <div className="flex flex-col gap-1 font-medium text-center">
+                <h3 className="text-dSecondary text-base sm:text-xl">
+                  {position}
+                </h3>
+                <p className="text-dPrimary/80 text-sm">{duration}</p>
+              </div>
+              <ul className="flex flex-col self-start gap-5 list-inside list-disc text-dPrimary text-xs sm:text-base">
+                {tasks.map((task, index) => {
+                  return (
+                    <li
+                      key={index}
+                      dangerouslySetInnerHTML={{ __html: task }}
+                    />
+                  );
+                })}
+              </ul>
+            </Card>
+          );
+        })}
+      </div>
+    </Layout>
+  );
+};
+
+export default Experience;
