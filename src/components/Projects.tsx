5c5196000560b8f9330c82b2b30969a624788d42
Rafael Mojica <rafael.mojica27@gmail.com>
2024-04-23 13:31:45 -0500

    feat: add common MainTitle at components

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Layout from "@/common/Layout";
 import Project from "@/common/Project";
+import MainTitle from "@/common/MainTitle";
 import { PROJECTS } from "@/data/project";
 
 const Projects = () => {
   return (
     <Layout id="projects">
-      <h2 className="text-center text-secondary dark:text-dark-secondary font-bold pb-20">
-        Proyectos
-      </h2>
+      <MainTitle title="Proyectos"/>
       <div className="flex flex-col gap-5">
         {PROJECTS.map((project) => {
           return <Project key={project.name} {...project} />;
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,14 +1,13 @@
+import React from "react";
 import Layout from "@/common/Layout";
+import MainTitle from "@/common/MainTitle";
 import IconButton from "@/common/IconButton";
 import { STACK } from "@/data/stack";
-import React from "react";
 
 const Skills = () => {
   return (
     <Layout id="skills" className="pb-12 md:pb-16">
-      <h2 className="text-center text-secondary dark:text-dark-secondary font-bold py-20">
-        Habilidades
-      </h2>
+      <MainTitle title="Habilidades" />
       <div className="max-w-5xl m-auto">
         <div className="flex flex-wrap justify-center items-center gap-5">
           {STACK.map(({ name, href, Icon }) => {
