80385d5aafab8dcd60c3ea75469e2787d174e741
Rafael Mojica <rafael.mojica27@gmail.com>
2024-04-25 12:50:26 -0500

    fix: opacity bg footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import { SOCIAL_NETWORKS } from "@/data/socialNetworks";
 
 const Footer = () => {
   return (
-    <footer className=" bg-dPrimary/10 mt-11 md:mt-20 p-6">
+    <footer className=" bg-dPrimary/5 mt-11 md:mt-20 p-6">
       <Layout
         id="footer"
         className="flex flex-col gap-6 justify-center items-center"
